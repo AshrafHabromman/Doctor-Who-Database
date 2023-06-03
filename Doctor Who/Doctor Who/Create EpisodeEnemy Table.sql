4fb6f0bee8d8406471e8eab312a0dd01a932ffe6
AshrafHabromman <ashraf.habrumman@gmail.com>
2023-06-03 23:36:12 +0300

    Edit on EpisodeEnemy file, one of constraints name

--- Doctor Who/Doctor Who/Create EpisodeEnemy Table.sql
+++ Doctor Who/Doctor Who/Create EpisodeEnemy Table.sql
@@ -2,6 +2,6 @@ USE "Doctor Who"
 
 CREATE TABLE tblEpisodeEnemy(
 EpisodeEnemyId INT NOT NULL PRIMARY KEY,  
-EpisodeId INT  CONSTRAINT constraintEpisodeForeignKey REFERENCES tblEpisode(EpisodeId),
+EpisodeId INT  CONSTRAINT constraintEpisodeEnemyForeignKey REFERENCES tblEpisode(EpisodeId),
 EnemyId INT CONSTRAINT constraintEnemyForeignKey REFERENCES tblEnemy(EnemyId)
 )
