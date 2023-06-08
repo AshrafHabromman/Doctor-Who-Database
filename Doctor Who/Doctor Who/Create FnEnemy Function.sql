716bd26e0e7d373ddfcfa9297ddc038ebcdac414
AshrafHabromman <ashraf.habrumman@gmail.com>
2023-06-08 23:06:23 +0300

    Create the FnEnemies function

---
+++ Doctor Who/Doctor Who/Create FnEnemy Function.sql
@@ -0,0 +1,21 @@
+USE "Doctor Who"
+Go
+
+CREATE FUNCTION dbo.fnEnemies(@EpisodeId INT)
+RETURNS VARCHAR(255)
+AS
+BEGIN 
+	DECLARE @enemies VARCHAR(255)
+	SET @enemies = ''
+
+	
+	SELECT @enemies = COALESCE(@enemies + ', ', '') +  E.EnemyName 
+	FROM tblEnemy E
+	WHERE E.EnemyId IN (
+		SELECT DISTINCT EE.EnemyId
+		FROM tblEpisodeEnemy EE
+		WHERE EE.EpisodeId = @EpisodeId
+	)
+
+	RETURN @enemies
+END
--- Doctor Who/Doctor Who/Use Created Functions .sql
+++ Doctor Who/Doctor Who/Use Created Functions .sql
@@ -2,3 +2,6 @@ USE "Doctor Who"
 
 SELECT  dbo.fnCompanions(5) AS CompanionNames
 
+Go 
+
+SELECT  dbo.fnEnemies(3) AS EnemiesNames
